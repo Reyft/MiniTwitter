22027f0a1eee707982659d85bf6df74dbb44dc9f
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-05-24 09:29:14 +0200

    Mise en page

--- src/serveur/User.java
+++ src/serveur/User.java
@@ -8,48 +8,48 @@ import java.util.List;
 
 @SuppressWarnings("serial")
 public class User extends UnicastRemoteObject implements Serializable, UserInterface {
-	private String name;
-	private String password;
-	private List<String> abo;
+    private String name;
+    private String password;
+    private List<String> abo;
 
-	public User() throws RemoteException{
-		this("anonymous", "admin");
-	}
+    public User () throws RemoteException {
+        this("anonymous", "admin");
+    }
 
-	public User(String name, String password) throws RemoteException{
-		super();
-		this.name = name;
-		this.password = password;
-		abo = new ArrayList<String>();
-	}
-	
-	@Override
-	public String getPassword(){
-		return password;
-	}
-	
-	@Override
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	@Override
-	public String getName() {
-		return name;
-	}
-	
-	@Override
-	public List<String> getAbo(){
-		return abo;
-	}
-	
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
+    public User (String name, String password) throws RemoteException {
+        super();
+        this.name = name;
+        this.password = password;
+        abo = new ArrayList<String>();
+    }
 
-	@Override
-	public void addAbo(String name) throws RemoteException {
-		abo.add(name);
-	}	
+    @Override
+    public String getPassword () {
+        return password;
+    }
+
+    @Override
+    public void setPassword (String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getName () {
+        return name;
+    }
+
+    @Override
+    public List<String> getAbo () {
+        return abo;
+    }
+
+    @Override
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void addAbo (String name) throws RemoteException {
+        abo.add(name);
+    }
 }
